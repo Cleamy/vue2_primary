a8f797aabec9c3441a4d677578481e11d9867a1e
Cleamy <1670550395@qq.com>
2024-09-02 19:07:48 +0800

    first commit

---
+++ 01_初识vue/20、单文件组件/School.vue
@@ -0,0 +1,37 @@
+
+<template>
+    <!-- 组件的结构 -->
+     <div class="demo">
+        <h2>名称：{{schoolname}}</h2>
+        <h2>地址：{{address}}</h2>
+        <button @click="showName">tip name</button>
+
+     </div>
+</template>
+
+
+<script>
+    // 组件交互相关的代码
+    export default {
+        name:'School',
+        data(){
+            return {
+                schoolname:'sname',
+                address:'address'
+            }
+        },
+        methods:{
+            showName(){
+                console.log(this.schoolname)
+            }
+        }
+    }
+</script>
+
+
+<style>
+    /* 组件的样式 */
+    .demo {
+        background-color: antiquewhite;
+    }
+</style>
---
+++ 01_初识vue/20、单文件组件/Student.vue
@@ -0,0 +1,31 @@
+
+<template>
+    <!-- 组件的结构 -->
+     <div class="demo">
+        <h2>名字：{{studentname}}</h2>
+        <h2>年龄：{{age}}</h2>
+        
+
+     </div>
+</template>
+
+
+<script>
+    // 组件交互相关的代码
+    export default {
+        name:'student',
+        data(){
+            return {
+                studentname:'stname',
+                age: 21
+            }
+        },
+       
+    }
+</script>
+
+
+<style>
+    /* 组件的样式 */
+   
+</style>
